baf3330e09ff8dd537a9b2b4d0af341705af758e
pinati1 <789ori1@gmail.com>
2026-05-17 10:53:54 +0300

    add deck init and shuffle

--- src/logic.py
+++ src/logic.py
@@ -1,10 +1,10 @@
 from assets.blackjack_art import logo
-
+import player
 class BlackjackGame:
     def __init__(self):
         print(logo)
         print("Welcome to Blackjack!")
 
     def start(self):
-        # TODO: Implement game loop
+        
         pass
