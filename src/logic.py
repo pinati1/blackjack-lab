af95509092c75f87aa983a0e5127a4b15afe1736
pinati1 <789ori1@gmail.com>
2026-05-18 15:18:32 +0300

    add state and game loop logic

--- src/logic.py
+++ src/logic.py
@@ -1,9 +1,9 @@
 from sre_parse import State
 
 from assets.blackjack_art import logo
-from player import Player,Dealer
-from deck import Deck
-
+from src.player import Player, Dealer
+from src.deck import Deck
+from src.state import State
 
 class BlackjackGame:
     def __init__(self):
@@ -12,25 +12,20 @@ class BlackjackGame:
         self.deck = Deck()
         self.player = Player("player1")
         self.dealer = Dealer()
-        self.state = State()
-        self.round = 0
-
+        self.state = State(self.player, self.dealer)
 
     def draw_card(self):
         return self.deck.cards.pop(0)
 
-
-    def hit(self,player):
+    def hit(self, player):
 
         new_card = self.draw_card()
-        if new_card.suit is 'Ace':
-            if self.player.sum+11>21:
-                self.player.sum+=1
+        if new_card.rank == 'Ace':
+            if player.sum + 11 > 21:
+                player.sum += 1
             else:
-                self.player.sum += new_card.value
-        self.player.hand.append(new_card)
-
-
+                player.sum += new_card.value
+        player.hand.append(new_card)
 
     def deal_cards(self):
         for i in range(2):
@@ -39,17 +34,26 @@ class BlackjackGame:
 
     def start(self):
         self.deal_cards()
-        #check if won
         print(f"your cards: {self.player.hand}")
         print(f"your sum: {self.player.sum}")
         print(f"dealer cards: {self.dealer.hand[0]}")
+        if self.state.check_state():
+            print(f"{self.state.state}")
+            return
         choice = input("do you want another card? (y/n)")
-        while choice is 'y':
+        while choice == 'y':
             self.hit(self.player)
             print(f"your cards: {self.player.hand}")
             print(f"your sum: {self.player.sum}")
+            if self.state.check_state():
+                print(f"{self.state.state}")
+                return
             choice = input("do you want another card? (y/n)")
-        while self.dealer.sum<17:
+        while self.dealer.sum < 17:
             self.hit(self.dealer)
+            if self.state.check_state():
+                print(f"{self.state.state}")
+                return
+        print(f"{self.state.state}")
 
-        pass
+
--- src/state.py
+++ src/state.py
@@ -1,24 +1,38 @@
-from player import Player
+
+
 class State:
-    def __init__(self,player,dealer):
+    def __init__(self, player, dealer):
         self.player = player
         self.dealer = dealer
-        
+        self.state = ""
 
-    def is_tie(self):
-        if self.player.sum == 21 and self.dealer.sum == 21:
+    def is_tie(self) -> bool:
+        return self.player.sum == 21 and self.dealer.sum == 21
+
+    def is_player_won(self) -> bool:
+        return self.player.sum == 21 and not self.dealer.sum == 21
+
+    def is_dealer_won(self) -> bool:
+        return self.dealer.sum == 21 and not self.player.sum == 21
+
+    def is_busted(self, player):
+        if player.sum > 21:
+            self.state = "busted"
+
+    def check_win(self):
+        if self.is_player_won():
+            self.state = "player won"
+            return
+        if self.is_dealer_won():
+            self.state = "dealer won"
+
+    def is_end_game(self) -> bool:
+        return self.state is not None
+
+    def check_state(self) -> bool:
+
+        self.check_win()
+        if self.is_end_game():
             return True
-        return False
-
-
-    def is_win(self):
-        if self.player.sum > 21 and not self.is_tie():
-            return True
-        return False
-
-
-    def is_busted(self):
-        if self.player.sum > 21:
-            self.player.is_busted = True
-        if self.dealer.sum > 21:
-            self.dealer.is_busted = True
+        self.is_tie()
+        return self.is_end_game()
